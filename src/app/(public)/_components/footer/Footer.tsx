79e6bb97adcc3ca3632d41ee20461237eddb8765
Olis Solutions <solutionsbyoli@gmail.com>
2024-12-03 00:17:41 -0800

    minor footer improvements

--- src/app/(public)/_components/footer/Footer.tsx
+++ src/app/(public)/_components/footer/Footer.tsx
@@ -8,27 +8,15 @@ import { motion } from "motion/react";
 
 export default function Footer() {
   return (
-    <footer className="bg-neutral-900 flex p-8 justify-between items-start">
-      <div className="-mt-2 flex flex-col gap-2">
-        <Logo />
-        <p className="text-neutral-300 font-bold tracking-tighter">
-          Your home is our canvas
-        </p>
-        <p>CA Lic. {companyInfo.caLicense}</p>
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
-        <ul className="flex flex-col gap-4">
-          <h3 className="text-neutral-200 font-bold">Get In Touch</h3>
-          {companyInfo.contact.map((contact) => (
-            <li
-              key={contact.type}
-              className="text-neutral-300 flex gap-2 items-center text-md"
-            >
-              <contact.icon size={20} />
-              {contact.value}
-            </li>
-          ))}
-          <li className="flex gap-2 items-center">
+    <footer className="bg-neutral-900">
+      <div className="flex h-full w-full justify-between  p-8">
+        <div className="flex flex-col gap-4 h-auto">
+          <Logo />
+          <p className="text-neutral-300 font-bold tracking-tighter">
+            Your home is our canvas
+          </p>
+          <p>CA Lic. {companyInfo.caLicense}</p>
+          <li className="flex gap-2 items-center mt-auto">
             {companyInfo.socials.map((social) => (
               <motion.a
                 whileHover={{ scale: 1.1 }}
@@ -41,24 +29,41 @@ export default function Footer() {
               </motion.a>
             ))}
           </li>
-        </ul>
-        <div className="flex flex-col gap-4">
-          <h3 className="text-neutral-200 font-bold">Hours</h3>
-          {companyInfo.hours.map((hour) => (
-            <p key={hour.days} className="text-neutral-300">
-              {hour.days}: {hour.open} - {hour.close}
-            </p>
-          ))}
         </div>
-        <div className="flex flex-col gap-4">
-          <h3 className="text-neutral-200 font-bold">Quick Links</h3>
-          <ul className="text-neutral-400 flex flex-col gap-4">
-            <FooterLink href="/" label="Home" />
-            <FooterLink href="/about" label="About" />
-            <FooterLink href="/projects" label="Projects" />
-            <FooterLink href="/services" label="Services" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
+          <ul className="flex flex-col gap-4">
+            <h3 className="text-neutral-200 font-bold">Get In Touch</h3>
+            {companyInfo.contact.map((contact) => (
+              <li
+                key={contact.type}
+                className="text-neutral-300 flex gap-2 items-center text-md"
+              >
+                <contact.icon size={20} />
+                {contact.value}
+              </li>
+            ))}
           </ul>
+          <div className="flex flex-col gap-4">
+            <h3 className="text-neutral-200 font-bold">Hours</h3>
+            {companyInfo.hours.map((hour) => (
+              <p key={hour.days} className="text-neutral-300">
+                {hour.days}: {hour.open} - {hour.close}
+              </p>
+            ))}
+          </div>
+          <div className="flex flex-col gap-4">
+            <h3 className="text-neutral-200 font-bold">Quick Links</h3>
+            <ul className="text-neutral-400 flex flex-col gap-4">
+              <FooterLink href="/" label="Home" />
+              <FooterLink href="/about" label="About" />
+              <FooterLink href="/projects" label="Projects" />
+              <FooterLink href="/services" label="Services" />
+            </ul>
+          </div>
         </div>
+      </div>
+      <div className="flex justify-center p-2 bg-black text-sm text-neutral-400">
+        © ModCon Builders Inc 2024. All rights reserved.
       </div>
     </footer>
   );
